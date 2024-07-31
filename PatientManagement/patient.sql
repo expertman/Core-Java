fdda3a7646bde200fe3e2b88ec48b26a1f62ccfc
Henry <javaexpert@nate.com>
2024-07-31 17:03:50 +0900

    환자관리프로그램 수정

--- PatientManagement/patient.sql
+++ PatientManagement/patient.sql
@@ -9,3 +9,44 @@ CREATE TABLE mycompany.Patient(
 	money          INT,
 	CONSTRAINT patient_number_pk PRIMARY KEY(number)
 );
+
+Delimiter $$
+CREATE PROCEDURE sp_select_all_patient()
+BEGIN
+	SELECT number, dept, operfee, hospitalfee, money
+    FROM Patient
+    ORDER BY number DESC;
+END $$
+Delimiter ;
+
+Delimiter //
+CREATE PROCEDURE sp_select_one_patient(IN v_number TINYINT)
+BEGIN
+	SELECT *
+    FROM Patient
+    WHERE number = v_number;
+END //
+Delimiter ;
+
+
+Delimiter //
+CREATE PROCEDURE  sp_update_patient
+(
+	IN    v_number     TINYINT,
+    IN    v_code        CHAR(2),
+    IN    v_days         SMALLINT,
+    IN    v_age          TiNYINT,
+    IN    v_dept         VARCHAR(20),
+    IN    v_operfee      INT,
+    IN    v_hospitalfee   INT,
+    IN    v_money      INT
+)
+BEGIN
+	UPDATE Patient
+    SET code = v_code, days = v_days,  age = v_age, 
+           dept = v_dept, operfee = v_operfee, hospitalfee = v_hospitalfee,
+           money = v_money
+    WHERE number = v_number;
+    COMMIT;
+END //
+Delimiter ;
--- PatientManagement/src/com/example/model/PatientVO.java
+++ PatientManagement/src/com/example/model/PatientVO.java
@@ -61,13 +61,25 @@ public class PatientVO {
 	public String getCode() {
 		return code;
 	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
 
 	public int getDays() {
 		return days;
 	}
+	
+	public void setDays(int days) {
+		this.days = days;
+	}
 
 	public int getAge() {
 		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	@Override 
